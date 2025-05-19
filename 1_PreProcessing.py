5c4c7c3a69c7f1138927ce2db2063f1732de58d1
Malek BOUMEDINE <malek.boumedine@gmail.com>
2025-05-19 16:40:28 +0200

    nettoyage des données OK

--- 1_PreProcessing.py
+++ 1_PreProcessing.py
@@ -1,29 +1,81 @@
 import pandas as pd
 import numpy as np
-import missingno as msno
+
+
+########################
+# NETTOYAGE DES DONNEES
+########################
 
 pd.set_option('display.max_columns', 500)
 
 df = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
+df.columns = df.columns.str.replace(" ", "", regex=False)
 
-msno.matrix(df)
+print("shape : ", df.shape)
 
 print(df.dtypes)
 
 print(df.select_dtypes(include="object").head())
 
+#  colonne TotalCharges : suppression des espaces, remplacement des vides par des NaN, conversion et type float
 df["TotalCharges"] = df["TotalCharges"].str.strip().replace(",", ".", regex=True).replace("", np.nan).astype(float)
 df["TotalCharges"].isna().sum()
+df.dropna(inplace=True)
 
+print("shape : ", df.shape)
+
+boolean_cols = []
+cat_cols = []
+numeric_cols = df.select_dtypes(include="number").columns.tolist()
+target = "Churn"
+
+
+# colonnes objet : 
 for c in df.select_dtypes(include="object").columns :
     df[c] = df[c].str.strip().replace("", np.nan)
-    print(f"valeurs uniques de {c} : {len(df[c].unique())} ")
-    
-    
-for c in df.select_dtypes(include="object").columns :
-    print(f"valeurs uniques de {c} : {df[c].unique()} ")
-    
-    
+    print(f"# {c} \n valeurs uniques : {len(df[c].unique())}") 
+    if len(df[c].unique()) == 2 : 
+        boolean_cols.append(c)
+    if len(df[c].unique()) > 2 : 
+        cat_cols.append(c)
+    print(f"nombre de NaN : {df[c].isna().sum()}")
+    print(f"valeurs uniques : {df[c].unique()}")
+    print()
+
+
+# colonnes numériques :
+for c in df.select_dtypes(exclude="object").columns :
+    print(f"# {c} \n valeurs uniques : {len(df[c].unique())}") 
+    if len(df[c].unique()) == 2 : 
+        boolean_cols.append(c)
+    print(f"nombre de NaN : {df[c].isna().sum()}")
+    print()    
+
+
+print("#"*50)
+print("colonnes yes no : ", boolean_cols)
+print("colonnes numeriques : ", numeric_cols)
+print("colonnes 3 valeurs minimum :", cat_cols)
+print("target :", target)
+
+#########
+#ENCODAGE
+#########
+# one hot encoder pour les variable categorielles
+
+
+
+
+##################
+# MISE A L'ECHELLE
+##################
+
+
+
+
+
+
+
 
 
 
